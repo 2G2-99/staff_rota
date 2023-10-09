f94715dc2570ff53008fdd0150a05236385592d4
Santiago <sagos0919@gmail.com>
2023-10-09 15:35:35 +0100

    Re organised the App component to include only the RouterProvider and include the content (Header, Outlet/matching child and Footer)  inside the Root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import { RouterProvider } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import router from './Router/routerConfig';
-import Header from './components/Body/Header';
-import Layout from './components/Body/Layout';
-import Footer from './components/Body/Footer';
 
 function App() {
 	return (
 		<>
-			<Header siteTitle={'Staff Rota'}>
-				<RouterProvider router={router} />
-				<TanStackRouterDevtools router={router} />
-			</Header>
-			<Layout />
-			<Footer />
+			<RouterProvider router={router} />
+			<TanStackRouterDevtools router={router} />
 		</>
 	);
 }
