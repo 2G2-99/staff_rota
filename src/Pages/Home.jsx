46e550beaaeae03b1a9dd290ea98a34dc98ddd77
Santiago <sagos0919@gmail.com>
2023-10-25 21:40:48 +0100

    Created the constants for the shifts times, refactored both checkShift and typeOfShift while renaming the constants on calculateShiftHours

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,3 @@
-import Week from '../classes/weekly/week';
 import {
 	currentShift,
 	formattedDate,
@@ -24,21 +23,7 @@ function Home() {
 
 // * Testing team member classes and methods
 // * Testing Week classes and methods
-const currentWeek = new Week(new Date());
-const oneWeekAhead = new Week(new Date(), 1);
-const twoWeeksAhead = new Week(new Date(), 2);
 
-console.log(currentWeek.startOn);
-console.log(currentWeek.days);
-console.log('==========================================');
-
-console.log(oneWeekAhead.startOn);
-console.log(oneWeekAhead.days);
-console.log('==========================================');
-
-console.log(twoWeeksAhead.startOn);
-console.log(twoWeeksAhead.days);
-console.log('==========================================');
 // * Testing Week classes and methods
 
 export default Home;
--- src/classes/weekly/shift.js
+++ src/classes/weekly/shift.js
@@ -1,3 +1,7 @@
+const START_OPENING = '07:00';
+const END_OPENING = '16:00';
+const START_CLOSING = '15:00';
+
 class Shift {
 	constructor(date, startOn, end, other = []) {
 		this.date = date;
@@ -6,48 +10,55 @@ class Shift {
 		this.other = other;
 		this.checkShift();
 		this.type = this.typeOfShift();
-		this.hours = this.calculateHours();
+		this.hours = this.calculateShiftHours();
 	}
 
+	/*
+	 * Checks that startOn and end values are given to the constructor and that the end of the shift goes no further than '00:30'
+	 */
 	checkShift() {
 		if (!this.startOn || !this.end) {
 			throw new Error('A shift must include a starting and ending time');
 		}
-		if (
-			this.end < this.startOn &&
-			this.end !== '00:00' &&
-			this.end !== '00:30'
-		) {
+		if (this.end < this.startOn && this.end > '00:30') {
 			throw new Error(
 				'You cannot set a shift where the starting time is ahead of the ending time'
 			);
 		}
 	}
 
+	/*
+	 * Evaluates both startOn and end time to determine the type of shift
+	 */
 	typeOfShift() {
-		if (this.startOn >= '15:00') {
-			return 'closing';
-		} else if (this.startOn >= '07:00' && this.end > '16:00') {
-			return 'split';
-		} else if (this.startOn >= '07:00') {
-			return 'opening';
-		}
+		return this.startOn >= START_CLOSING
+			? 'closing'
+			: this.startOn > START_OPENING && this.end > END_OPENING
+			? 'split'
+			: 'opening';
 	}
 
-	calculateHours() {
-		const startOn = new Date(this.date);
-		const end = new Date(this.date);
+	/*
+	 * Calculates the difference between the startOn and end to return the total hours of the shift. First a Date object is created and then modified to add the hours and minutes of the shift
+	 */
+	calculateShiftHours() {
+		const startDateObject = new Date(this.date);
+		const endDateObject = new Date(this.date);
+
 		const [startHours, startMinutes] = this.startOn.split(':').map(Number);
 		const [endHours, endMinutes] = this.end.split(':').map(Number);
 
-		startOn.setHours(startHours, startMinutes);
-		end.setHours(endHours, endMinutes);
+		startDateObject.setHours(startHours, startMinutes);
+		endDateObject.setHours(endHours, endMinutes);
 
-		if (end < startOn) {
-			end.setDate(end.getDate() + 1);
+		if (endDateObject < startDateObject) {
+			endDateObject.setDate(endDateObject.getDate() + 1);
 		}
 
-		return (end - startOn) / 1000 / 60 / 60;
+		/*
+		 * calculates the difference between the end and start times in milliseconds, then converts it to hours by dividing by 1000 (to get seconds), then 60 (to get minutes), then 60 again (to get hours)
+		 */
+		return (endDateObject - startDateObject) / 1000 / 60 / 60;
 	}
 }
 
