546d301f4c25889c3c81b471426f1bacccfc873e
Santiago <sagos0919@gmail.com>
2023-10-30 20:33:17 +0000

    Brought TeamMember and Manager classes back to a same file since there is a circular dependency between them

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import { TeamMember } from '../classes/team/teamMember';
 import Week from '../classes/weekly/week';
 import {
 	currentShift,
@@ -20,75 +21,17 @@ function Home() {
 	);
 }
 
-//* Testing team member classes and methods
-
-// * Testing team member classes and methods
 // * Testing Week classes and methods
 const currentWeek = new Week(new Date());
-
-currentWeek.days[0].addShift('07:00', '16:00');
-currentWeek.days[0].addShift('07:00', '16:00');
-currentWeek.days[0].addShift('07:00', '14:00');
-currentWeek.days[0].addShift('15:00', '00:00');
-currentWeek.days[0].addShift('16:00', '00:00');
-currentWeek.days[0].addShift('16:00', '00:00');
-
-currentWeek.days[1].addShift('07:00', '16:00');
-currentWeek.days[1].addShift('07:00', '16:00');
-currentWeek.days[1].addShift('07:00', '14:00');
-currentWeek.days[1].addShift('15:00', '00:00');
-currentWeek.days[1].addShift('16:00', '00:00');
-currentWeek.days[1].addShift('16:00', '00:00');
-
-currentWeek.days[2].addShift('07:00', '16:00');
-currentWeek.days[2].addShift('07:00', '16:00');
-currentWeek.days[2].addShift('07:00', '14:00');
-currentWeek.days[2].addShift('15:00', '00:00');
-currentWeek.days[2].addShift('16:00', '00:00');
-currentWeek.days[2].addShift('16:00', '00:00');
-
-currentWeek.days[3].addShift('07:00', '16:00');
-currentWeek.days[3].addShift('07:00', '16:00');
-currentWeek.days[3].addShift('07:00', '14:00');
-currentWeek.days[3].addShift('14:00', '22:00');
-currentWeek.days[3].addShift('15:00', '00:30');
-currentWeek.days[3].addShift('16:00', '00:30');
-currentWeek.days[3].addShift('18:00', '00:30');
-
-currentWeek.days[4].addShift('07:00', '16:00');
-currentWeek.days[4].addShift('07:00', '16:00');
-currentWeek.days[4].addShift('07:00', '16:00');
-currentWeek.days[4].addShift('12:00', '21:00');
-currentWeek.days[4].addShift('12:00', '21:00');
-currentWeek.days[4].addShift('15:00', '00:30');
-currentWeek.days[4].addShift('16:00', '00:30');
-currentWeek.days[4].addShift('16:00', '00:30');
-
-currentWeek.days[5].addShift('07:00', '16:00');
-currentWeek.days[5].addShift('07:00', '16:00');
-currentWeek.days[5].addShift('07:00', '14:00');
-currentWeek.days[5].addShift('12:00', '21:00');
-currentWeek.days[5].addShift('12:00', '21:00');
-currentWeek.days[5].addShift('15:00', '00:30');
-currentWeek.days[5].addShift('16:00', '00:30');
-currentWeek.days[5].addShift('16:00', '00:30');
-
-currentWeek.days[6].addShift('07:00', '16:00');
-currentWeek.days[6].addShift('07:00', '16:00');
-currentWeek.days[6].addShift('07:00', '14:00');
-currentWeek.days[6].addShift('12:00', '21:00');
-currentWeek.days[6].addShift('15:00', '00:00');
-currentWeek.days[6].addShift('16:00', '00:00');
-currentWeek.days[6].addShift('16:00', '00:00');
-
-// Hours used on Day [0]
-currentWeek.days[0].calculateHoursOfDay();
-console.log(currentWeek.days[0].hours);
-
-// Hours used on the whole week
-console.log(currentWeek.days);
-currentWeek.calculateHoursOfWeek();
-console.log(currentWeek.hours);
+const monday = currentWeek.days[0];
 // * Testing Week classes and methods
+//* Testing team member classes and methods
+const me = new TeamMember(9, 1, 1999, 'Santiago', 'Gomez', 'sm');
+
+console.log(me);
+console.log(monday.shifts);
+// * Testing team member classes and methods
 
 export default Home;
+
+// TODO: Implement a way to connect both TeamMember and Week classes.
--- src/classes/team/teamMember.js
+++ src/classes/team/teamMember.js
@@ -1,5 +1,4 @@
 import { differenceInYears } from 'date-fns/esm';
-import Manager from './manager';
 
 class TeamMember {
 	constructor(day, month, year, firstName, lastName, position) {
@@ -70,4 +69,28 @@ class TeamMember {
 	}
 }
 
-export default TeamMember;
+class Manager extends TeamMember {
+	constructor(day, month, year, firstName, lastName, position) {
+		super(day, month, year, firstName, lastName, position);
+		this.checkManager();
+	}
+
+	checkManager() {
+		const isAManager =
+			this.position === 'sm'
+				? true
+				: this.position === 'am'
+				? true
+				: this.position === 'gm';
+
+		if (isAManager) {
+			console.log('Welcome to the Team Member Modifier');
+		} else {
+			console.log(
+				'You are not a Manager. You are not allowed to make any modifications'
+			);
+		}
+	}
+}
+
+export { TeamMember, Manager };
