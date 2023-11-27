5a0a20373faca8e2c4c868df0f48a0b322db8bfb
Santiago <sagos0919@gmail.com>
2023-11-27 23:14:23 +0000

    Styles added to TableRota component

--- src/Pages/Rota.jsx
+++ src/Pages/Rota.jsx
@@ -1,17 +1,12 @@
 import Week from '../classes/weekly/week';
 import TableRota from '../components/Rota/TableRota';
+import '../styles/Rota.css';
 
 const CurrentWeek = new Week(new Date());
 
 function Rota() {
 	return (
 		<>
-			<h2
-				data-rota-date={`${CurrentWeek.days[0].formattedDate}`}
-				className='rota feature'
-			>
-				Week Rota {CurrentWeek.days[0].formattedDate}
-			</h2>
 			<TableRota selectedWeek={CurrentWeek} />
 		</>
 	);
--- src/components/Rota/TableRota.jsx
+++ src/components/Rota/TableRota.jsx
@@ -5,6 +5,12 @@ import TeamRows from './TeamRows';
 function TableRota({ selectedWeek }) {
 	return (
 		<table className='table feature'>
+			<caption
+				data-rota-date={`${selectedWeek.days[0].formattedDate}`}
+				className='title'
+			>
+				Week Rota {selectedWeek.days[0].formattedDate}
+			</caption>
 			<thead className='header'>
 				<tr className='row'>
 					<th className='heading team'>Team Member</th>
