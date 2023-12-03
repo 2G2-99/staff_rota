b61badf3308501bd7f8387ea0d2db19d1a6b8a3b
Santiago <sagos0919@gmail.com>
2023-12-04 00:27:23 +0200

    Added styles to the edit_group on the data cells to interact with the shifts

--- src/components/Rota/TableRota.jsx
+++ src/components/Rota/TableRota.jsx
@@ -1,20 +1,21 @@
 import team from '../../data/team';
 import DateCell from './DateCell';
 import TeamRows from './TeamRows';
+import '../../styles/Rota.css';
 
 function TableRota({ selectedWeek }) {
 	return (
-		<table className='table feature'>
-			<thead className='header'>
-				<tr className='row'>
-					<th className='heading team'>Team Member</th>
+		<table className="table feature">
+			<thead className="header">
+				<tr className="row">
+					<th className="heading team">Team Member</th>
 					<DateCell currentWeek={selectedWeek} />
 				</tr>
 			</thead>
-			<tbody className='body'>
+			<tbody className="body">
 				<TeamRows team={team} currentWeek={selectedWeek} />
 			</tbody>
-			<tfoot className='footer'></tfoot>
+			<tfoot className="footer"></tfoot>
 		</table>
 	);
 }
--- src/components/Rota/TeamRows.jsx
+++ src/components/Rota/TeamRows.jsx
@@ -1,10 +1,8 @@
-import Button from '../Button';
-import DeleteIcon from '../Icons/DeleteIcon';
-import EditIcon from '../Icons/EditIcon';
+import EditShift from './EditShift';
 
 function TeamRows({ team, currentWeek }) {
 	return team.map(teamMember => (
-		<tr key={teamMember.capitalisedName()} className='rota row'>
+		<tr key={teamMember.capitalisedName()} className="rota row">
 			<TeamMemberCell teamMember={teamMember} />
 			<ShiftCell teamMember={teamMember} currentWeek={currentWeek} />
 		</tr>
@@ -12,22 +10,14 @@ function TeamRows({ team, currentWeek }) {
 }
 
 function TeamMemberCell({ teamMember }) {
-	return <td className='rota cell name'>{teamMember.capitalisedName()}</td>;
+	return <td className="rota cell name">{teamMember.capitalisedName()}</td>;
 }
 
 function ShiftCell({ currentWeek, teamMember }) {
 	return currentWeek.days.map(day => (
-		<td key={day.dayName} className='rota cell shift'>
-			<p className='shift-data'>{day.shifts.get(teamMember)?.formatShift()}</p>
-
-			<div className='btn-container'>
-				<Button className={'shift edit'}>
-					<EditIcon />
-				</Button>
-				<Button className={'shift  delete'}>
-					<DeleteIcon />
-				</Button>
-			</div>
+		<td key={day.dayName} className="rota cell shift">
+			<p className="shift-data">{day.shifts.get(teamMember)?.formatShift()}</p>
+			<EditShift />
 		</td>
 	));
 }
