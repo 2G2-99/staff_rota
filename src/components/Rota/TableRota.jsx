843d866c5f4b9c08a51374e7c843036578ce437e
Santiago <sagos0919@gmail.com>
2023-12-11 11:10:58 +0000

    Table rota styled (still working on responsiveness)

--- src/components/Rota/TableRota.jsx
+++ src/components/Rota/TableRota.jsx
@@ -4,24 +4,26 @@ import TeamRows from './TeamRows';
 
 function TableRota({ selectedWeek }) {
 	return (
-		<table className='table feature'>
-			<caption
-				data-rota-date={`${selectedWeek.days[0].formattedDate}`}
-				className='title'
-			>
-				Week Rota {selectedWeek.days[0].formattedDate}
-			</caption>
-			<thead className='header'>
-				<tr className='row'>
-					<th className='heading team'>Team Member</th>
-					<DateCell currentWeek={selectedWeek} />
-				</tr>
-			</thead>
-			<tbody className='body'>
-				<TeamRows team={team} currentWeek={selectedWeek} />
-			</tbody>
-			<tfoot className='footer'></tfoot>
-		</table>
+		<div className='table-container feature'>
+			<table className='table'>
+				<caption
+					data-rota-date={`${selectedWeek.days[0].formattedDate}`}
+					className='title'
+				>
+					Week Rota {selectedWeek.days[0].formattedDate}
+				</caption>
+				<thead className='header'>
+					<tr className='row'>
+						<th className='heading team'>Team Member</th>
+						<DateCell currentWeek={selectedWeek} />
+					</tr>
+				</thead>
+				<tbody className='body'>
+					<TeamRows team={team} currentWeek={selectedWeek} />
+				</tbody>
+				<tfoot className='footer'></tfoot>
+			</table>
+		</div>
 	);
 }
 
